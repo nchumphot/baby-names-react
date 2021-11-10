fe382088ecaf6f17a13b8b1b4c20dd75cbdeee8a
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-11-10 11:41:23 +0000

    Added a favourites list

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -9,19 +9,36 @@ interface IBabyName {
 
 export function MainContent(): JSX.Element {
   const [search, setSearch] = useState<string>("");
+  const [favourites, setFavourites] = useState<IBabyName[]>([]);
 
   // Sort baby names alphabetically
   const allBabyNames = unsortedBabyNames.sort((a, b) =>
     a.name.toLowerCase().localeCompare(b.name.toLowerCase())
   );
 
-  // Return a button of baby name
+  // Return a normal button of baby name
   const BabyNameButton = (babyName: IBabyName): JSX.Element => {
     return (
       <button
         className={
           babyName.sex.toLowerCase() === "m" ? "button-male" : "button-female"
         }
+        onClick={() => setFavourites((favourites) => [...favourites, babyName])}
+      >
+        {babyName.name}
+      </button>
+    );
+  };
+
+  // Return a 'favourite' button
+  const FavouriteButton = (babyName: IBabyName): JSX.Element => {
+    const returnArray = favourites.filter((item) => item !== babyName && item);
+    return (
+      <button
+        className={
+          babyName.sex.toLowerCase() === "m" ? "button-male" : "button-female"
+        }
+        onClick={() => setFavourites(returnArray)}
       >
         {babyName.name}
       </button>
@@ -50,8 +67,21 @@ export function MainContent(): JSX.Element {
       <SearchBar />
       <br />
       <br />
+      {favourites.length === 0 ? (
+        <h2>Select your favourite baby names and they will appear here.</h2>
+      ) : (
+        <h2>Favourites:</h2>
+      )}
+      {favourites
+        .sort((a, b) =>
+          a.name.toLowerCase().localeCompare(b.name.toLowerCase())
+        )
+        .map(FavouriteButton)}
+      <br />
+      <br />
       {allBabyNames
         .filter((item) => {
+          // filter by search
           if (search === "") {
             return item;
           } else if (item.name.toLowerCase().includes(search.toLowerCase())) {
@@ -59,6 +89,10 @@ export function MainContent(): JSX.Element {
           } else {
             return null;
           }
+        })
+        .filter((item) => {
+          // do not show favourite names
+          return !favourites.includes(item) && item;
         })
         .map(BabyNameButton)}
     </>
